11c087b5148483c2b2ac90795345f646d61ec976
unknown <mctenenbaum@SY-T2110-34.SeneDS.senecacollege.ca>
2014-04-02 15:50:37 -0400

    MoeTestCommit

---
+++ VBAssign2/VBAssign2/Models/Adapters/Customer.vb
@@ -0,0 +1,11 @@
+﻿' Filename: /Models/Adapters/Customer
+' purpose: wrapper to communicate with the customer class in VBAssign1 dll
+
+Imports VBA01Lib
+
+
+Public Class Customer
+
+    'Moe's Comment
+
+End Class
