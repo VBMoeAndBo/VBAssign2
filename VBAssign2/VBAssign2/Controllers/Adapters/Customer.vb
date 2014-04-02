3441555a7cf2b1f455bcea54459395e00cc15df7
unknown <bliu54@SY-T2110-33.SeneDS.senecacollege.ca>
2014-04-02 15:49:29 -0400

    tst git

--- VBAssign2/VBAssign2/Controllers/Adapters/Customer.vb
+++
@@ -1,9 +0,0 @@
-﻿' Filename: /Models/Adapters/Customer
-' purpose: wrapper to communicate with the customer class in VBAssign1 dll
-
-Imports VBA01Lib
-
-
-Public Class Customer
-
-End Class
---
+++ VBAssign2/VBAssign2/Models/Adapters/Customer.vb
@@ -0,0 +1,9 @@
+﻿' Filename: /Models/Adapters/Customer
+' purpose: wrapper to communicate with the customer class in VBAssign1 dll
+
+Imports VBA01Lib
+
+' i am bo - test Git
+Public Class Customer
+
+End Class
